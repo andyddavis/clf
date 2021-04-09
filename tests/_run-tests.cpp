b8c08b04453a1efa5161c4e904553a665ab717a7
Andy Davis <davisad@alum.mit.edu>
2021-04-08 22:32:23 -0400

    added test script

---
+++ tests/_run-tests.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// include the google testing header
+//#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+  //::testing::InitGoogleTest(&argc, argv);
+
+  //const int res = RUN_ALL_TESTS();
+
+  //return res;
+}
