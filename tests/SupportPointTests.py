e0b158a59ae6951294130f6270f89800fef25e12
Andy Davis <davisad@alum.mit.edu>
2021-04-15 21:48:56 -0400

    added the support point python interface

--- tests/SupportPointTests.py
+++ tests/SupportPointTests.py
@@ -6,9 +6,25 @@ import CoupledLocalFunctions as clf
 
 class TestSupportPoint(unittest.TestCase):
     def test_create(self):
-        x = np.array([1.0, 4.0])
-        d1 = dict()
-        d1["Type"] = "TotalOrderPolynomials"
-        d = dict()
-        d["BasisFunctions"] = d1
-        point = clf.SupportPoint(x, d)
+        # some point
+        x = np.array([1.0, 4.0, 3.0, -2.0])
+
+        # options for the basis
+        basisOptions = dict()
+        basisOptions["Type"] = "TotalOrderPolynomials"
+        basisOptions["Order"] = 3
+
+        # options for the support point
+        ptOptions = dict()
+        ptOptions["BasisFunctions"] = basisOptions
+
+        # create the point
+        point = clf.SupportPoint(x, ptOptions)
+
+        # make sure the point is in the right place
+        self.assertEqual(len(point.x), len(x))
+        for i in range(len(x)):
+            self.assertAlmostEqual(point.x[i], x[i], places=10)
+
+        # make sure the basis was created
+        self.assertEqual(point.basis.NumBasisFunctions(), 35)
